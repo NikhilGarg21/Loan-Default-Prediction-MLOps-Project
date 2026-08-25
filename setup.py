d23707afc84d7194a5dd683d00b1b19b4823f08d
NikhilGarg21 <n.garg2102@gmail.com>
2026-08-25 19:20:12 +0530

    venv set up

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nikhil Garg",
+    author_email="nikhilgarg2126@gmail.com",
+    packages=find_packages()
+)
